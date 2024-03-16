d60cb523897dc01d22a3744c4c75e1cb1c3bd775
douglas emerick <dgrippemerick@gmail.com>
2024-03-16 11:15:47 -0300

    Add Entity

---
+++ src/TC.GrupoTrinta.BlogNews.Domain/Entity/Image.cs
@@ -0,0 +1,23 @@
+﻿
+using TC.GrupoTrinta.BlogNews.Domain.SeedWork;
+
+namespace TC.GrupoTrinta.BlogNews.Domain.Entity
+{
+    public class Image : AggregateRoot
+    {
+        public Guid ImageId { get; set; }
+        public Guid VideoId { get; set; }        
+        public string NameImage { get; set; }
+        public string PathImagem { get; set; }
+        public DateTime CreateAt { get; private set; }
+
+        public Image(Guid imageId, Guid videoId, string nameImage, string pathImagem, DateTime createAt)
+        {
+            ImageId = imageId;
+            VideoId = videoId;
+            NameImage = nameImage;
+            PathImagem = pathImagem;
+            CreateAt = createAt;
+        }
+    }
+}
---
+++ src/TC.GrupoTrinta.BlogNews.Domain/Entity/Video.cs
@@ -0,0 +1,22 @@
+﻿
+using TC.GrupoTrinta.BlogNews.Domain.SeedWork;
+
+namespace TC.GrupoTrinta.BlogNews.Domain.Entity
+{
+    public class Video : AggregateRoot
+    {
+        public Guid VideoId { get; set; }
+        public string NameZip { get; set; }
+        public string PathZip { get; set; }
+        public DateTime CreateAt { get; private set; }
+
+
+        public Video(Guid videoId, string nameZip, string pathZip, DateTime createAt) : base()
+        {
+            VideoId = videoId;
+            NameZip = nameZip;
+            PathZip = pathZip;
+            CreateAt = createAt;
+        }
+    }
+}
